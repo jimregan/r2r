8bb8fff03c569386944f4f8d43b66f556a7d1fc7
grindcrank <grindcrank@e05fb319-f5f2-429d-9119-215cbae6dd6c>
2011-03-14 19:07:50 +0000

    Added MultiFunctionFactory - an abstract FunctionFactory which allows for defining mulitple functions at once. Added XPathFunctionFactory - implements several string and numeric XPath functions.

--- src/test/java/de/fuberlin/wiwiss/r2r/functions/Helper.java
+++ src/test/java/de/fuberlin/wiwiss/r2r/functions/Helper.java
@@ -1,15 +1,20 @@
 package de.fuberlin.wiwiss.r2r.functions;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Helper {
-	protected static List<List<String>> getArgumentLists(String... args) {
+	public static List<List<String>> getArgumentLists(String... args) {
 		List<List<String>> argumentLists = new ArrayList<List<String>>();
-		for(String arg: args) {
-			List<String> a1 = new ArrayList<String>();
-			a1.add(arg);
-			argumentLists.add(a1);
+		for (String arg : args) {
+			if (arg != null) {
+				List<String> a1 = new ArrayList<String>();
+				a1.add(arg);
+				argumentLists.add(a1);
+			} else {
+				argumentLists.add(Collections.EMPTY_LIST);
+			}
 		}
 
 		return argumentLists;
