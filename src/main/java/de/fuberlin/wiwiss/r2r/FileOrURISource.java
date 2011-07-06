bb1e183b89444633a3e80049d3271832de9ef2b8
Andreas Schultz <andreas-schultz@users.noreply.github.com>
2011-07-06 14:01:25 +0000

    Added (mapping) validation method for Repository class

--- src/main/java/de/fuberlin/wiwiss/r2r/FileOrURISource.java
+++ src/main/java/de/fuberlin/wiwiss/r2r/FileOrURISource.java
@@ -4,8 +4,14 @@ import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.shared.NotFoundException;
 import com.hp.hpl.jena.util.FileManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Implementation of the Source Interface which reads a file or URI resource into memory.
@@ -24,6 +30,25 @@ public class FileOrURISource implements Source {
 		FileManager fileManager = FileManager.get();
 		model = fileManager.loadModel(fileOrURI);
 	}
+
+    /**
+     *
+     * @param file Can either be a file or a directory. In case of a directory, all files in this directory all loaded.
+     * @throws NotFoundException
+     */
+    public FileOrURISource(File file) throws IOException {
+        model = ModelFactory.createDefaultModel();
+        if(file.isDirectory()) {
+            for(File mappingFile: file.listFiles()) {
+                FileManager fileManager = FileManager.get();
+		        Model mappingFileModel = fileManager.loadModel(mappingFile.getCanonicalPath());
+                model.add(mappingFileModel);
+            }
+        } else {
+            FileManager fileManager = FileManager.get();
+		    model = fileManager.loadModel(file.getCanonicalPath());
+        }
+    }
 	
 	public QueryExecution executeQuery(String query) {
 		return QueryExecutionFactory.create(query, model);
