c981a22d05d0bd8a3009d62f04d9c66810e25fa6
fatorange <fatorange@users.sourceforge.net>
2011-11-21 11:32:34 +0000

    Added Apache 2.0 license headers

--- src/main/java/de/fuberlin/wiwiss/r2r/parser/TargetPatternLexer.java
+++ src/main/java/de/fuberlin/wiwiss/r2r/parser/TargetPatternLexer.java
@@ -1,3 +1,20 @@
+/*
+ *  R2R
+ *
+ *  Copyright 2010-2011 Freie Universität Berlin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 // $ANTLR 3.2 Sep 23, 2009 12:02:23 /home/andreas/code/mavenprojects/mapping/SourceForger2rApi/r2r/antlr-files/TargetPattern.g 2011-05-11 12:46:18
 
   package de.fuberlin.wiwiss.r2r.parser;
